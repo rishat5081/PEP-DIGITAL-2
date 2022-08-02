af42c937c5e1a306bf46cdcf4867a4df4af185c7
Saad <rishat.5081@gmail.com>
2022-08-03 00:49:53 +0500

    change the port

--- app.js
+++ app.js
@@ -611,9 +611,9 @@ const getMenu = async (roleName) => {
   });
 };
 
-server.listen(80, () => {
+server.listen(3000, () => {
   console.log(`\x1b[42m--------------------------------------\x1b[0m`);
-  console.log(`\n \x1b[32m Node Server Listening at: 80 \n \x1b[0m`);
+  console.log(`\n \x1b[32m Node Server Listening at: 3000 \n \x1b[0m`);
   console.log(`\x1b[42m--------------------------------------\x1b[0m\n`);
   console.log("Node Memory Status: ", process.memoryUsage());
 });
